53ba2ed225e1fe4c36d59619d5f41d9a37780ed6
kairos4213 <jamesdkairos@gmail.com>
2024-12-01 12:44:39 -0600

    Add metric tracking middleware

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
 
+	cfg := &apiConfig{fileserverHits: atomic.Int32{}}
+
+	fServer := http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot)))
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux.Handle("/app/", cfg.middleWareMetricsInc(fServer))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", cfg.handlerPrintMetrics)
+	mux.HandleFunc("/reset", cfg.handlerResetMetrics)
 
 	server := &http.Server{
 		Handler: mux,
@@ -23,6 +31,33 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middleWareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerPrintMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	serverHits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %v", serverHits)))
+}
+
+func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Server hit metrics reset!"))
 }
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
