211c1c22fe693089dd6b0af0e96f47e1f9a56095
kairos4213 <jamesdkairos@gmail.com>
2024-12-01 11:02:10 -0600

    Add handlerReadiness

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	server := &http.Server{
 		Handler: mux,
@@ -22,4 +23,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
